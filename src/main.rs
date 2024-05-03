a6e33794f3715b6c875d1a7389658207affe8742
Jacek programmer <jacek301@gmail.com>
2024-05-03 16:00:16 +0200

    Find the smallest integer in the array Kata Start

--- src/main.rs
+++ src/main.rs
@@ -2,39 +2,14 @@ fn main() {
     println!("Hello, world!");
 }
 
-fn count_positives_sum_negatives(input: Vec<i32>) -> Vec<i32> {
+fn find_smallest_int(arr: &[i32]) -> i32 {
 
-    if input.is_empty() {
-        return vec![];
-    }
-
-    let sum_of_negative = input.iter().filter( |&&x| x < 0).sum();
-    let count_of_positive = input.iter().filter( |&&x| x > 0).count() as i32;
-
-
-    return vec![count_of_positive,sum_of_negative]
+    return 0;
 }
 
-
-#[cfg(test)]
-mod tests {
-    use super::count_positives_sum_negatives;
-
-    fn dotest(a: &[i32], expected: &[i32]) {
-        let actual = count_positives_sum_negatives(a.to_vec());
-        assert_eq!(actual, expected, "With input = {a:?}\nExpected {expected:?} but got {actual:?}")
-    }
-    #[test]
-    fn fixed_tests() {
-        dotest(&[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], &[10, -65]);
-        dotest(&[], &[]);
-        dotest(&[0, 2, 3, 0, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14], &[8, -50]);
-        dotest(&[0,1,2,3,4,5], &[5, 0]);
-        dotest(&[1,2,3,4,5], &[5, 0]);
-        dotest(&[0,-1,-2,-3,-4,-5], &[0, -15]);
-        dotest(&[-1,-2,-3,-4,-5], &[0, -15]);
-        dotest(&[0,0,0,0], &[0,0]);
-        dotest(&[0], &[0,0]);
-    }
+#[test]
+fn sample_tests() {
+    assert_eq!(find_smallest_int(&[34, 15, 88, 2]), 2);
+    assert_eq!(find_smallest_int(&[34, -345, -1, 100]), -345);
 }
 
