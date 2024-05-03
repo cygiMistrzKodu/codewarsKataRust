8a23a94ee9a12f51ee3a36ff217eb71c80805fdc
Jacek programmer <jacek301@gmail.com>
2024-05-03 23:25:22 +0200

    L1: Set Alarm Kata Start

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,20 @@ fn main() {
     println!("Hello, world!");
 }
 
-fn find_smallest_int(arr: &[i32]) -> i32 {
-
-    return *arr.iter().min().unwrap();
-
+fn set_alarm(employed: bool, vacation: bool) -> bool {
+    todo!();
 }
 
-#[test]
-fn sample_tests() {
-    assert_eq!(find_smallest_int(&[34, 15, 88, 2]), 2);
-    assert_eq!(find_smallest_int(&[34, -345, -1, 100]), -345);
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_set_alarm() {
+        assert_eq!(set_alarm(true, true), false, "Fails when input is true, true");
+        assert_eq!(set_alarm(false, true), false, "Fails when input is false, true");
+        assert_eq!(set_alarm(false, false), false, "Fails when input is false, false");
+        assert_eq!(set_alarm(true, false), true, "Fails when input is true, false");
+    }
 }
 
