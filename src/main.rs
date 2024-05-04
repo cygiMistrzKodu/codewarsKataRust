c391218c8a9c7b0b6850f3a73540082594f5ef5b
Jacek programmer <jacek301@gmail.com>
2024-05-05 00:01:42 +0200

    Total amount of points Kata Start

--- src/main.rs
+++ src/main.rs
@@ -2,24 +2,29 @@ fn main() {
     println!("Hello, world!");
 }
 
-fn invert(values: &[i32]) -> Vec<i32> {
-    let mut inverted_values: Vec<i32> = Vec::new();
-
-    for number in values {
-        inverted_values.push(number * -1)
-    }
-    return inverted_values;
+fn points(games: &[String]) -> u32 {
+    todo!()
 }
 
 #[cfg(test)]
 mod tests {
-    use super::invert;
+    use super::points;
+
+    const ERR_MSG: &str = "\nYour result (left) did not match the expected output (right)";
+
+    fn do_fixed_test(e: &[&str], expected: u32) {
+        let a = &e.iter().map(|x| x.to_string()).collect::<Vec<_>>();
+        assert_eq!(points(a), expected, "{ERR_MSG} with games = {a:?}")
+    }
 
     #[test]
-    fn basic_tests() {
-        assert_eq!(invert(&vec![1, 2, 3, 4, 5]), vec![-1, -2, -3, -4, -5]);
-        assert_eq!(invert(&vec![1, -2, 3, -4, 5]), vec![-1, 2, -3, 4, -5]);
-        assert_eq!(invert(&vec![]), vec![]);
+    fn fixed_tests() {
+        do_fixed_test(&["1:0", "2:0", "3:0", "4:0", "2:1", "3:1", "4:1", "3:2", "4:2", "4:3"], 30);
+        do_fixed_test(&["1:1", "2:2", "3:3", "4:4", "2:2", "3:3", "4:4", "3:3", "4:4", "4:4"], 10);
+        do_fixed_test(&["0:1", "0:2", "0:3", "0:4", "1:2", "1:3", "1:4", "2:3", "2:4", "3:4"], 0);
+        do_fixed_test(&["1:0", "2:0", "3:0", "4:0", "2:1", "1:3", "1:4", "2:3", "2:4", "3:4"], 15);
+        do_fixed_test(&["1:0", "2:0", "3:0", "4:4", "2:2", "3:3", "1:4", "2:3", "2:4", "3:4"], 12);
     }
 }
+
 
