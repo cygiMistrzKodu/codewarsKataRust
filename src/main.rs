93f5d729bf348805b064b8e08d995df52ec90f20
Jacek programmer <jacek301@gmail.com>
2024-05-05 18:37:46 +0200

    Returning Strings Kata Start

--- src/main.rs
+++ src/main.rs
@@ -2,26 +2,21 @@ fn main() {
     println!("Hello, world!");
 }
 
-fn add_binary(a: u64, b: u64) -> String {
-    return format!("{:b}", a + b);
+fn greet(name: &str) -> String {
+    todo!()
 }
 
 #[cfg(test)]
 mod tests {
-    use super::add_binary;
-
-    fn dotest(a: u64, b: u64, expected: &str) {
-        let actual = add_binary(a, b);
-        assert_eq!(actual, expected, "With a = {a}, b = {b}\nExpected \"{expected}\" but got \"{actual}\"")
-    }
+    use super::*;
 
     #[test]
-    fn sample_tests() {
-        dotest(1, 1, "10");
-        dotest(0, 1, "1");
-        dotest(1, 0, "1");
-        dotest(2, 2, "100");
-        dotest(51, 12, "111111");
+    fn basic_tests() {
+        assert_eq!(greet("Ryan"), "Hello, Ryan how are you doing today?");
+        assert_eq!(
+            greet("Shingles"),
+            "Hello, Shingles how are you doing today?"
+        );
     }
 }
 
