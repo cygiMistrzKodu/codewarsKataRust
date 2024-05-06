4c44fd5f9c61422d259370cbbe2685d5c6beb0f7
Jacek programmer <jacek301@gmail.com>
2024-05-07 00:29:13 +0200

    Beginner Series #2 Clock Kata Start

--- src/main.rs
+++ src/main.rs
@@ -2,38 +2,24 @@ fn main() {
     println!("Hello, world!");
 }
 
-fn series_sum(n: u32) -> String {
-    if n == 0 {
-        return "0.00".to_string();
-    }
+fn past(h: i32, m: i32, s: i32) -> i32 {
 
-    let mut sum_of_series: f64 = 1f64;
-    let mut divider: f64 = 4f64;
-    for _index in 0..n - 1 {
-        sum_of_series = sum_of_series + 1f64 / divider;
-        divider += 3f64;
-    }
-
-    return format!("{:.2}", sum_of_series);
+    // your code here
+    return 0;
 }
+
 
 #[cfg(test)]
 mod tests {
-    use super::series_sum;
-
-    fn test(input: u32, expected: &str) {
-        let actual = series_sum(input);
-        assert_eq!(actual, expected, "Expected series_sum({input}) to be {expected}, but was {actual}");
-    }
+    use super::*;
 
     #[test]
-    fn sample_tests() {
-        test(1, "1.00");
-        test(2, "1.25");
-        test(3, "1.39");
-        test(7, "1.68");
-        test(39, "2.26");
-        test(0, "0.00");
+    fn basic_tests() {
+        assert_eq!(past(0, 1, 1), 61000);
+        assert_eq!(past(1, 1, 1), 3661000);
+        assert_eq!(past(0, 0, 0), 0);
+        assert_eq!(past(1, 0, 1), 3601000);
+        assert_eq!(past(1, 0, 0), 3600000);
     }
 }
 
