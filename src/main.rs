332f2bed5b937cafaf6979ef1846ee967f686d53
Jacek programmer <jacek301@gmail.com>
2024-05-08 22:10:53 +0200

    Abbreviate a Two Word Name Kata Start

--- src/main.rs
+++ src/main.rs
@@ -3,25 +3,18 @@ fn main() {
     println!("Hello, world!");
 }
 
-fn make_negative(n: i32) -> i32 {
-    return if n < 0 {
-        n
-    } else {
-        n * -1
-    }
+fn abbrev_name(name: &str) -> String {
+
+    return "".to_string()
 }
 
-
-#[cfg(test)]
-mod tests {
-    use super::make_negative;
-
-    #[test]
-    fn sample_tests() {
-        assert_eq!(make_negative(1), -1);
-        assert_eq!(make_negative(-5), -5);
-        assert_eq!(make_negative(0), 0);
-    }
+#[test]
+fn sample_tests() {
+    assert_eq!(abbrev_name("Sam Harris"), "S.H");
+    assert_eq!(abbrev_name("Patrick Feenan"), "P.F");
+    assert_eq!(abbrev_name("Evan Cole"), "E.C");
+    assert_eq!(abbrev_name("P Favuzzi"), "P.F");
+    assert_eq!(abbrev_name("David Mendieta"), "D.M");
 }
 
 
