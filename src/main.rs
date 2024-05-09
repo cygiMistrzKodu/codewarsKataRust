fd00ab5605c7881a57200c87388d06b714effa78
Jacek programmer <jacek301@gmail.com>
2024-05-10 01:32:28 +0200

    The Feast of Many Beasts kata start

--- src/main.rs
+++ src/main.rs
@@ -3,26 +3,17 @@ fn main() {
     println!("Hello, world!");
 }
 
-fn enough(cap: i32, on: i32, wait: i32) -> i32 {
-    let free_space_in_bus = cap - on;
-    let people_cant_take = wait - free_space_in_bus;
+fn feast(beast: &str, dish: &str) -> bool {
 
-    return if people_cant_take > 0 {
-        people_cant_take
-    } else { 0 };
+    todo!()
 }
 
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_enough() {
-        assert_eq!(enough(10, 5, 5), 0, "enough(10, 5, 5) should return 0");
-        assert_eq!(enough(100, 60, 50), 10, "enough(100, 60, 50) should return 10");
-        assert_eq!(enough(20, 5, 5), 0, "enough(20, 5, 5) should return 0");
-    }
+#[test]
+fn sample_test_cases() {
+    assert_eq!(feast("great blue heron", "garlic naan"), true);
+    assert_eq!(feast("chickadee", "chocolate cake"), true);
+    assert_eq!(feast("brown bear", "bear claw"), false);
 }
 
 
