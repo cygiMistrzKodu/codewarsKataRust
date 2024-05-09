e7c3f81518223c3e20ae05d0b0dc882bb1851087
Jacek programmer <jacek301@gmail.com>
2024-05-09 21:36:49 +0200

    Abbreviate a Two Word Name Kata Solution

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use regex::Regex;
 
 fn main() {
     println!("Hello, world!");
@@ -5,7 +6,17 @@ fn main() {
 
 fn abbrev_name(name: &str) -> String {
 
-    return "".to_string()
+    let first_word_letters_regex = Regex::new(r"\b[a-zA-z]").unwrap();
+
+    let mut initials = String::with_capacity(0);
+    for capture_first_word_letter in first_word_letters_regex.captures_iter(name) {
+        let letter = &capture_first_word_letter[0];
+        initials.push_str(&*letter.to_uppercase());
+        initials.push_str(".");
+    }
+    initials.pop();
+
+    return initials;
 }
 
 #[test]
@@ -15,6 +26,8 @@ fn sample_tests() {
     assert_eq!(abbrev_name("Evan Cole"), "E.C");
     assert_eq!(abbrev_name("P Favuzzi"), "P.F");
     assert_eq!(abbrev_name("David Mendieta"), "D.M");
+    assert_eq!(abbrev_name("george clooney"), "G.C");
+    assert_eq!(abbrev_name("Sam x"), "S.X");
 }
 
 
