835f2d3b2dace39cca4835b3d6d000684be15253
Jacek programmer <jacek301@gmail.com>
2024-05-12 14:55:17 +0200

    Grasshopper - Messi goals function Kata solution

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ fn main() {
 }
 
 fn goals(la_liga_goals: i32, champions_league_goals: i32, copa_del_rey_goals: i32) -> i32 {
-    todo!()
+    return  la_liga_goals + champions_league_goals + copa_del_rey_goals;
 }
 
 
