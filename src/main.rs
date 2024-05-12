ef8bcab98e0ffef5dfc853fbbbb9010513b89d0a
Jacek programmer <jacek301@gmail.com>
2024-05-12 23:39:55 +0200

    Two Sum Kata Solution

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,17 @@ fn main() {
 }
 
 fn two_sum(numbers: &[i32], target: i32) -> (usize, usize) {
-    todo!()
+    for (index, number) in numbers.iter().enumerate() {
+        for (index_next, number_next) in numbers.iter().enumerate() {
+            if index != index_next {
+                if number + number_next == target {
+                    return (index, index_next);
+                }
+            }
+        }
+    }
+
+    return (0, 0);
 }
 
 
@@ -26,19 +36,11 @@ mod tests {
             "\nnumbers: {:?}\ntarget: {}\nresult: {:?}\nresult tuple has an index out of bounds",
             nums, sum, user_tuple
         );
-        assert!(
-            user_tuple.0 != user_tuple.1,
-            "\nnumbers: {:?}\ntarget: {}\nresult: {:?}\nresult tuple must have two different indices",
-            nums, sum, user_tuple
-        );
+        assert_ne!(user_tuple.0, user_tuple.1, "\nnumbers: {:?}\ntarget: {}\nresult: {:?}\nresult tuple must have two different indices", nums, sum, user_tuple);
         let num1 = nums[user_tuple.0];
         let num2 = nums[user_tuple.1];
         let user_sum = num1 + num2;
-        assert!(
-            user_sum == sum,
-            "\nnumbers: {:?}\ntarget: {}\nresult: {:?}\nnumber as index {}: {}\nnumber as index {}: {}\nsum of the two numbers: {}\nsum of the two numbers did not equal target",
-            nums, sum, user_tuple, user_tuple.0, num1, user_tuple.1, num2, user_sum
-        )
+        assert_eq!(user_sum, sum, "\nnumbers: {:?}\ntarget: {}\nresult: {:?}\nnumber as index {}: {}\nnumber as index {}: {}\nsum of the two numbers: {}\nsum of the two numbers did not equal target", nums, sum, user_tuple, user_tuple.0, num1, user_tuple.1, num2, user_sum)
     }
 }
 
