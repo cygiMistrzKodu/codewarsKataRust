8f050f9b2dcc90a159cf42b6042290a5e49c9c59
Jacek programmer <jacek301@gmail.com>
2024-05-13 13:18:07 +0200

    Write Number in Expanded Form kata solution

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,22 @@ fn main() {
 }
 
 fn expanded_form(n: u64) -> String {
-    todo!()
+    let mut number_in_expanded_form: Vec<String> = Vec::new();
+    let mut number_to_expand = n as i64;
+
+
+    while number_to_expand > 0 {
+
+        let count_number_of_zeros = number_to_expand.to_string().chars().count() - 1;
+        let number_of_zeros =  count_number_of_zeros as u32;
+        let one_with_right_zeros = i64::pow(10, number_of_zeros);
+        let first_digit_on_left= number_to_expand / one_with_right_zeros;
+        let first_digit_with_zeros = first_digit_on_left * one_with_right_zeros;
+        number_in_expanded_form.push((first_digit_with_zeros).to_string());
+        number_to_expand = number_to_expand - first_digit_with_zeros;
+    }
+
+    return number_in_expanded_form.join(" + ");
 }
 
 
